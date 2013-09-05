971ec073307e20be7c803fde1205a55530220903
Ooms Aurélien <genius@B14-SR5511BE.(none)>
2013-09-05 14:33:03 +0200

    install manager

---
+++ install.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+		
+class CommandBuilder:
+	
+	def __init__(self,dir):
+		self.dir = dir + '/'
+			
+	def parse(self, output):
+		pipe = os.popen('ls -1 "' + self.dir + '"')
+		scripts = pipe.read()
+		pipe.close()
+		scripts = scripts.strip()
+		scripts = scripts.split("\n")
+		for file in scripts:
+			if os.path.isdir(self.dir+file):
+				sub = CommandBuilder(self.dir+file)
+				sub.parse(output)
+
+			else:
+				output[file] = self.dir+file;
+
+	
+if __name__ == '__main__':
+
+	register = {}
+	CommandBuilder('part').parse(register)
+
+	if(len(sys.argv) == 1):
+		for key in register:
+			#print [register[key]]
+			subprocess.call([register[key]])
+
+	else:
+		for i in range(1, len(sys.argv)):
+			#print [register[sys.argv[i]]]
+			subprocess.call([register[sys.argv[i]]])
